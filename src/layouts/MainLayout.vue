df948b3107b6e8a14435ff1bc55bf9e289e956c2
Cute-Fox <74364811+Cute-Fox@users.noreply.github.com>
2020-11-13 10:53:03 +0300

    ferst

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar-title>
+        Приложение тест
+      </q-toolbar-title>
+        <div>Тест {{ $q.version }}</div>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-drawer show-if-above v-model="left" side="left" bordered>
+      <div class="text-subtitle2 text-weight-bold text-center">Ваша доставка</div>
+        <div class="q-pa-md">
+          <q-linear-progress size="10px" :value="progress" />
+
+          <q-linear-progress rounded size="20px" :value="progress" color="warning" class="q-mt-20" />
+
+          <q-linear-progress rounded size="15px" :value="progress" color="secondary" class="q-mt-sm" />
+
+          <q-linear-progress size="25px" :value="progress" color="accent" class="q-mt-sm" />
+        </div>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      progress: 0.5,
+      left: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .progressBar1 {
+    size: 5 px;
+  }
+</style>
