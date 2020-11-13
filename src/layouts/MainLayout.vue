8cd6da69a934a8f017d8ea23b6bbc2f5dc17a28a
Cute-Fox <74364811+Cute-Fox@users.noreply.github.com>
2020-11-13 17:46:44 +0300

    17:46

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,5 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated container style="height: 100px">
-      <q-toolbar>
-        <div class="avatar1">
-          <q-avatar rounded size="58px" font-size="35px" color="teal" text-color="white" icon="N" class="absolute-center"/>
-        </div>
-      </q-toolbar>
-    </q-header>
-
+  <q-layout view="lHh lpR lFf">
     <q-page-container>
       <router-view />
     </q-page-container>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,19 +1,49 @@
 <template>
   <q-layout view="hHh lpR fFf">
 
-    <q-drawer show-if-above v-model="left" side="left" bordered>
-      <div class="text-subtitle2 text-weight-bold text-center">Ваша доставка</div>
+    <div class="fit row wrap justify-start items-start content-start">
+      <div class="col-3 bg-green">
+        <div class="row">
+          <div class="ava1">
+            <q-avatar rounded size="65px" font-size="42px" color="red" icon="F" text-color="white"/>
+          </div>
+          <div class="text-subtitle2 text-weight-bold margine">{{name}} <br> {{sorname}} </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <div class="full-width text-subtitle2 text-weight-bold text-center">
+              Ваша доставка
+            </div>
+            <div class="q-pa-md">
+              <q-linear-progress rounded size="15px" :value="progress" color="warning" class="q-mt-20" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col bg-blue">
+        <div class="full-width text-subtitle2 text-weight-bold text-center">Ваша доставка</div>
         <div class="q-pa-md">
           <q-linear-progress rounded size="15px" :value="progress" color="warning" class="q-mt-20" />
         </div>
-    </q-drawer>
+      </div>
+    </div>
+    <!-- <div class="leftMenu">
+      <q-drawer show-if-above v-model="left" side="left" bordered>
+        <div class="profile">
+          <div id="avatar">
+            <q-avatar rounded size="65px" font-size="42px" color="red" icon="F" text-color="white"/>
+          </div>
+        </div>
+        <div class="text-subtitle2 text-weight-bold text-center">Ваша доставка</div>
+          <div class="q-pa-md">
+            <q-linear-progress rounded size="15px" :value="progress" color="warning" class="q-mt-20" />
+          </div>
+      </q-drawer>
+    </div>
+ -->
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-
-    <!-- Катя - пишешет дизайн основного меню
-    Таня - ищещь во всех файлах упомнание Quasar (То что выводится) -->
+    <!-- Таня - пишешет дизайн основного меню
+    Катя - ищещь во всех файлах упомнание Quasar (То что выводится) -->
 
   </q-layout>
 </template>
@@ -23,7 +53,9 @@ export default {
   data () {
     return {
       progress: 0.84,
-      left: false
+      left: false,
+      name: 'Ник',
+      sorname: 'Уайлд'
     }
   }
 }
@@ -32,5 +64,14 @@ export default {
 <style scoped>
   .progressBar1 {
     size: 5 px;
+  }
+  .ava1 {
+    margin: 9px;
+  }
+  .leftMenu {
+    size: 150px;
+  }
+  .margine {
+    margin: 21px;
   }
 </style>
